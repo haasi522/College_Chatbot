a0fb01977215c7f8724cf5e5d0b018eb750da927
haasi522 <haasika.01@gmail.com>
2025-02-28 11:20:12 +0530

    Update college.py

--- college.py
+++ college.py
@@ -10,7 +10,7 @@ st.set_page_config(page_title="College Chatbot 🎓", page_icon="🎓", layout="
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-csv_url = "svcew_deatils.csv"
+csv_url = "svcew_details.csv"
 
 try:
     df = pd.read_csv(csv_url)
